752fa13157a541f1c3795d4bc9e434be04629e57
AJ Ruckman <40143425+ajruckman@users.noreply.github.com>
2021-06-02 11:20:16 -0400

    Add InputAdapters for using inputs with different types than those specified in an Structure member

---
+++ Integrant4.Fundament/InputAdapter.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Threading.Tasks;
+using Integrant4.API;
+using Microsoft.AspNetCore.Components;
+
+namespace Integrant4.Fundament
+{
+    public class InputAdapter<TIn, TOut> : IInput<TOut>
+    {
+        public delegate TOut AdaptTo(TIn? from);
+
+        private readonly IInput<TIn> _input;
+        private readonly AdaptTo     _to;
+
+        public InputAdapter(IInput<TIn> input, AdaptTo to)
+        {
+            _input = input;
+            _to    = to;
+
+            _input.OnChange += v => OnChange?.Invoke(_to.Invoke(v));
+        }
+
+        public RenderFragment Renderer() => _input.Renderer();
+
+        public async Task<TOut?> GetValue()
+        {
+            TIn? from = await _input.GetValue();
+            return _to.Invoke(from);
+        }
+
+        public event Action<TOut?>? OnChange;
+    }
+
+    public class RefreshableInputAdapter<TIn, TOut> : IRefreshableInput<TOut>
+    {
+        public delegate TOut AdaptTo(TIn? from);
+
+        private readonly IRefreshableInput<TIn> _input;
+        private readonly AdaptTo                _to;
+
+        public RefreshableInputAdapter(IRefreshableInput<TIn> input, AdaptTo to)
+        {
+            _input = input;
+            _to    = to;
+
+            _input.OnChange += v => OnChange?.Invoke(_to.Invoke(v));
+        }
+
+        public void Refresh() => _input.Refresh();
+
+        public RenderFragment Renderer() => _input.Renderer();
+
+        public async Task<TOut?> GetValue()
+        {
+            TIn? from = await _input.GetValue();
+            return _to.Invoke(from);
+        }
+
+        public event Action<TOut?>? OnChange;
+    }
+
+    public class WritableInputAdapter<TIn, TOut> : IWritableInput<TOut>
+    {
+        public delegate TOut AdaptTo(TIn? from);
+
+        public delegate TIn AdaptFrom(TOut? to);
+
+        private readonly IWritableInput<TIn> _input;
+        private readonly AdaptTo             _to;
+        private readonly AdaptFrom           _from;
+
+        public WritableInputAdapter(IWritableInput<TIn> input, AdaptTo to, AdaptFrom from)
+        {
+            _input = input;
+            _to    = to;
+            _from  = from;
+
+            _input.OnChange += v => OnChange?.Invoke(_to.Invoke(v));
+        }
+
+        public Task SetValue(TOut? value) => _input.SetValue(_from.Invoke(value));
+
+        public RenderFragment Renderer() => _input.Renderer();
+
+        public async Task<TOut?> GetValue()
+        {
+            TIn? from = await _input.GetValue();
+            return _to.Invoke(from);
+        }
+
+        public event Action<TOut?>? OnChange;
+    }
+
+    public class WritableRefreshableInputAdapter<TIn, TOut> : IWritableRefreshableInput<TOut>
+    {
+        public delegate TOut AdaptTo(TIn? from);
+
+        public delegate TIn AdaptFrom(TOut? to);
+
+        private readonly IWritableRefreshableInput<TIn> _input;
+        private readonly AdaptTo                        _to;
+        private readonly AdaptFrom                      _from;
+
+        public WritableRefreshableInputAdapter(IWritableRefreshableInput<TIn> input, AdaptTo to, AdaptFrom from)
+        {
+            _input = input;
+            _to    = to;
+            _from  = @from;
+
+            _input.OnChange += v => OnChange?.Invoke(_to.Invoke(v));
+        }
+
+        public void Refresh() => _input.Refresh();
+
+        public Task SetValue(TOut? value) => _input.SetValue(_from.Invoke(value));
+
+        public RenderFragment Renderer() => _input.Renderer();
+
+        public async Task<TOut?> GetValue()
+        {
+            TIn? from = await _input.GetValue();
+            return _to.Invoke(from);
+        }
+
+        public event Action<TOut?>? OnChange;
+    }
+}
